d0d43784e8bfd181e6754e946d9b386ed08bf5b5
Ivan Shevchenko <aivsitedev@gmail.com>
2025-04-18 00:20:30 +0300

    fixes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const session = require('express-session');
-const AWS = require('aws-sdk');
-const { Issuer, generators } = require('openid-client');
-
-const app = express();
+const express = require("express");
+const session = require("express-session");
+const AWS = require("aws-sdk");
+const { Issuer, generators } = require("openid-client");
 
 const AWS_REGION = process.env.REGION;
 const SECRET_ID = process.env.SECRET_ID;
@@ -13,62 +11,67 @@ const SERVICE_ENDPOINT = process.env.SERVICE_ENDPOINT;
 const CALLBACK_URI = process.env.CALLBACK_URI;
 const LOGOUT_URI = process.env.LOGOUT_URI;
 
-const secretsManager = new AWS.SecretsManager({ region: AWS_REGION });
+(async () => {
+  const app = express();
+  const secretsManager = new AWS.SecretsManager({ region: AWS_REGION });
 
-async function getCognitoSecrets() {
-    const data = await secretsManager.getSecretValue({
-        SecretId: SECRET_ID
-    }).promise();
+  async function getCognitoSecrets() {
+    const data = await secretsManager
+      .getSecretValue({
+        SecretId: SECRET_ID,
+      })
+      .promise();
 
     const secret = JSON.parse(data.SecretString);
     return secret;
-}
+  }
 
+  let creds = await getCognitoSecrets();
 
-let creds = await getCognitoSecrets();
+  console.log(creds.client_id);
 
-console.log(creds.client_id);
-
-let client;
-async function initializeClient() {
+  let client;
+  async function initializeClient() {
     const issuer = await Issuer.discover(COGNITO_ISSUER_URL);
     client = new issuer.Client({
-        client_id: creds.client_id,
-        client_secret: creds.client_secret,
-        redirect_uris: [CALLBACK_URI],
-        response_types: ['code']
+      client_id: creds.client_id,
+      client_secret: creds.client_secret,
+      redirect_uris: [CALLBACK_URI],
+      response_types: ["code"],
     });
-};
+  }
 
-app.set('view engine', 'ejs');
-initializeClient().catch(console.error);
+  app.set("view engine", "ejs");
+  initializeClient().catch(console.error);
 
-app.use(session({
-    secret: creds.session_secret,
-    resave: false,
-    saveUninitialized: false
-}));
+  app.use(
+    session({
+      secret: creds.session_secret,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
 
-const checkAuth = (req, res, next) => {
+  const checkAuth = (req, res, next) => {
     console.log(req.session.userInfo);
     if (!req.session.userInfo) {
-        req.isAuthenticated = false;
+      req.isAuthenticated = false;
     } else {
-        req.isAuthenticated = true;
+      req.isAuthenticated = true;
     }
     next();
-};
+  };
 
-app.get('/', checkAuth, (req, res) => {
-    res.render('home', {
-        isAuthenticated: req.isAuthenticated,
-        userInfo: req.session.userInfo,
-        accessToken: req.session.accessToken,
-        gatewayEndpoint: SERVICE_ENDPOINT
+  app.get("/", checkAuth, (req, res) => {
+    res.render("home", {
+      isAuthenticated: req.isAuthenticated,
+      userInfo: req.session.userInfo,
+      accessToken: req.session.accessToken,
+      gatewayEndpoint: SERVICE_ENDPOINT,
     });
-});
+  });
 
-app.get('/login', (req, res) => {
+  app.get("/login", (req, res) => {
     const nonce = generators.nonce();
     const state = generators.state();
 
@@ -76,47 +79,44 @@ app.get('/login', (req, res) => {
     req.session.state = state;
 
     const authUrl = client.authorizationUrl({
-        scope: 'email openid phone',
-        state: state,
-        nonce: nonce,
+      scope: "email openid phone",
+      state: state,
+      nonce: nonce,
     });
 
     res.redirect(authUrl);
-});
+  });
 
-app.get('/callback', async (req, res) => {
+  app.get("/callback", async (req, res) => {
     try {
-        const params = client.callbackParams(req);
-        const tokenSet = await client.callback(
-            CALLBACK_URI,
-            params,
-            {
-                nonce: req.session.nonce,
-                state: req.session.state
-            }
-        );
+      const params = client.callbackParams(req);
+      const tokenSet = await client.callback(CALLBACK_URI, params, {
+        nonce: req.session.nonce,
+        state: req.session.state,
+      });
 
-        const userInfo = await client.userinfo(tokenSet.access_token);
-        
-        req.session.userInfo = userInfo;
-        req.session.accessToken = tokenSet.access_token; // this is actually insane but nvm
+      const userInfo = await client.userinfo(tokenSet.access_token);
 
-        res.redirect('/');
+      req.session.userInfo = userInfo;
+      req.session.accessToken = tokenSet.access_token; // this is actually insane but nvm
+
+      res.redirect("/");
     } catch (err) {
-        console.error('Callback error:', err);
-        res.redirect('/');
+      console.error("Callback error:", err);
+      res.redirect("/");
     }
-});
+  });
 
-app.get('/logout', (req, res) => {
+  app.get("/logout", (req, res) => {
     req.session.destroy();
     const logoutUrl = LOGOUT_URI;
     res.redirect(logoutUrl);
-});
+  });
 
-app.listen(3000, '0.0.0.0', () => {
+  app.listen(3000, "0.0.0.0", () => {
     console.log(`Server is running`);
-});
+  });
+})();
 
 // const express = require('express')
 // const app = express()
@@ -129,4 +129,3 @@ app.listen(3000, '0.0.0.0', () => {
 // app.listen(port, '0.0.0.0', () => {
 //   console.log(`Example app listening on port ${port}`)
 // })
-
